bdf84ad1289aeb179672fe32cbbbc6f1707e9641
gpurch <girjeshpaliwal880@gmail.com>
2022-03-08 15:53:41 +0530

    commit

--- Crypto_profit_viewerbygirijesh/app/src/main/java/com/gdev/crypto_profit_viewerbygirijesh/rvm.java
+++ Crypto_profit_viewerbygirijesh/app/src/main/java/com/gdev/crypto_profit_viewerbygirijesh/rvm.java
@@ -1,64 +1,61 @@
 package com.gdev.crypto_profit_viewerbygirijesh;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link rvm#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+
+import com.lukelorusso.verticalseekbar.VerticalSeekBar;
+
+import java.util.ArrayList;
+
+
 public class rvm extends Fragment {
+    VerticalSeekBar v1,v2,v3,v4,v5,v6;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public rvm() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment rvm.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static rvm newInstance(String param1, String param2) {
-        rvm fragment = new rvm();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rvm, container, false);
+
+
+
+
+
+
+
+View v =  inflater.inflate(R.layout.fragment_rvm, container, false);
+            // initializing variable for bar chart.
+        v1 = v.findViewById(R.id.a1);
+        v2 = v.findViewById(R.id.a2);
+        v3 = v.findViewById(R.id.a3);
+        v4 = v.findViewById(R.id.a4);
+        v5 = v.findViewById(R.id.a5);
+        v6 = v.findViewById(R.id.a6);
+
+try{
+        v1.setProgress(10);
+       v2.setProgress(10);
+       v3.setProgress(10);
+       v4.setProgress(10);
+       v5.setProgress(10);
+       v6.setProgress(10);}
+catch (Exception e){
+    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+}
+
+            return  v;
     }
 }
